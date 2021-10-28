a0b2786a49394d24aa9bdebfdc6e20354033326f
pablojg9 <pablojuniorgn2@gmail.com>
2021-10-28 11:28:21 -0300

    Spring MVC - Thymeleaf - postgreSQL

--- src/main/java/com/project/rest/controller/AccountController.java
+++ src/main/java/com/project/rest/controller/AccountController.java
@@ -4,7 +4,6 @@ import com.project.rest.model.Account;
 import com.project.rest.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
