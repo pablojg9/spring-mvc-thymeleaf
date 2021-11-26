b4538e89b05d5192a4204f4b3f873b9757bffb5f
pablojg9 <pablojuniorgn2@gmail.com>
2021-11-26 17:28:30 -0300

    validations model

--- src/main/java/com/project/rest/model/Account.java
+++ src/main/java/com/project/rest/model/Account.java
@@ -1,11 +1,14 @@
 package com.project.rest.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Objects;
@@ -19,11 +22,16 @@ public class Account implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull(message = "O nome não pode ser nulo")
+    @NotEmpty(message = "o nome não pode ser vazio")
     private String username;
+
+    @NotEmpty()
     private String login;
     private String password;
 
-    @OneToMany(mappedBy = "account")
+    @OneToMany(mappedBy = "account", orphanRemoval = true, cascade = CascadeType.ALL)
     private List<Phone> phoneList;
 
     public Account(){}
