464937551de7b83dcb4b816098ff2f566e5d1c11
pablojg9 <pablojuniorgn2@gmail.com>
2021-12-09 12:45:05 -0300

    edit class

--- src/main/java/com/project/rest/entities/Account.java
+++ src/main/java/com/project/rest/entities/Account.java
@@ -1,12 +1,6 @@
 package com.project.rest.entities;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -34,7 +28,6 @@ public class Account implements Serializable {
 
     @NotNull(message = "A senha não pode ser nulo!")
     @NotEmpty(message = "A senha não pode estar vazio!")
-    @Min(value = 4, message = "a senha ter que ter acima de 4 caracteres!")
     private String password;
 
     @OneToMany(mappedBy = "account", orphanRemoval = true, cascade = CascadeType.ALL)
